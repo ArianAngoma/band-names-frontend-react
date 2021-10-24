fe2c87a7843013185929a94372e943c644a301be
unknown <arian.angoma@tecsup.edu.pe>
2021-10-24 03:32:21 -0500

    Configuración de Socket.io en React

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import {useEffect, useState} from 'react';
+import io from 'socket.io-client';
+
 /* Importaciones propias */
 import {BandAdd} from './components/BandAdd';
 import {BandList} from './components/BandList';
 
+const connectSocketServer = () => {
+    return io.connect('http://localhost:4000', {
+        transports: ['websocket']
+    });
+}
+
 function App() {
+    const [socket] = useState(connectSocketServer());
+    const [online, setOnline] = useState(false);
+
+    useEffect(() => {
+        // console.log(socket);
+        setOnline(socket.connected)
+    }, [socket]);
+
+    useEffect(() => {
+        socket.on('connect', () => [
+            setOnline(true)
+        ]);
+    }, [socket]);
+
+    useEffect(() => {
+        socket.on('disconnect', () => [
+            setOnline(false)
+        ]);
+    }, [socket])
+
     return (
         <div className="container">
             <div className="alert">
                 <p>
                     Service status:
-                    <span className="text-success"> Online</span>
-                    <span className="text-danger"> Offline</span>
+
+                    {
+                        (online)
+                            ? <span className="text-success"> Online</span>
+                            : <span className="text-danger"> Offline</span>
+                    }
                 </p>
             </div>
 
