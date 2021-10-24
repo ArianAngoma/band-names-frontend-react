4d166ead1eeae0c122faade8f3dc5c5679951378
unknown <arian.angoma@tecsup.edu.pe>
2021-10-24 03:15:55 -0500

    Inicio del proyecto

---
+++ src/components/BandAdd.js
@@ -0,0 +1,11 @@
+export const BandAdd = () => {
+    return (
+        <>
+            <h3>Agregar Banda</h3>
+
+            <form>
+                <input type="text" className="form-control" placeholder="Nuevo nombre de banda"/>
+            </form>
+        </>
+    )
+}
